c2a08096c932e001ce125ce5ec061d4aa3ae4fc8
김지현 <wlguslove12345@gmail.com>
2021-11-21 19:14:31 +0900

    add 앱아이콘, 폰트, 폴더 정리, 라이브러리 추가(Alamofire, Moya, Kingfisher)

---
+++ OOD_iOS/OOD_iOS/Asset/AppDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OOD_iOS
+//
+//  Created by 김지현 on 2021/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OOD_iOS/OOD_iOS/Screen/Signup/ViewController/SignupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupViewController.swift
+//  OOD_iOS
+//
+//  Created by 김지현 on 2021/11/21.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
